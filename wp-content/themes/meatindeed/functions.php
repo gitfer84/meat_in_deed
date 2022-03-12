62df4cf147f59cd456c18236468e4a13500152af
Fractal Prog <apple@Apples-MacBook-Pro.local>
2022-03-12 10:25:51 -0600

    Se modifican derechos reservados footer

--- wp-content/themes/meatindeed/functions.php
+++ wp-content/themes/meatindeed/functions.php
@@ -3,3 +3,19 @@ function meat_in_deed_files(){
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600;700&display=swap' );
 }
 add_action('wp_enqueue_scripts', 'meat_in_deed_files');
+
+// Modificar el footer
+
+function meatindeed_creditos(){
+    remove_action('storefront_footer', 'storefront_credit', 20 );
+    add_action('storefront_after_footer', 'meatindeed_nuevo_footer', 20);
+}
+
+add_action('init', 'meatindeed_creditos');
+
+function meatindeed_nuevo_footer(){
+    echo '<div class="reservados">';
+    echo    "Derechos Reservados &copy " . get_bloginfo('name') . " " .  date('Y');
+    echo '</div>';
+
+}
